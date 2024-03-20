941fffd51516e6171f7d634968dc9d96811e60f5
Roberto Rojo <rrojo.va@gmail.com>
2024-03-20 18:57:29 +0100

    Initial Commit

---
+++ CoinGeko/CoinGekoApp.swift
@@ -0,0 +1,32 @@
+//
+//  CoinGekoApp.swift
+//  CoinGeko
+//
+//  Created by Roberto Rojo Sahuquillo on 20/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CoinGekoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
